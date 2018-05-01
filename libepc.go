9a4ac0f36061a4d657d2b48dc9edd6b5cd8ce3a4
ma2ma <steve2008.ma@gmail.com>
2018-05-01 21:50:52 +0800

    修复小bug

--- libepc.go
+++ libepc.go
@@ -14,7 +14,7 @@ func Encode96bit(source string) (string, int, error) {
 	if str_len < 1 || str_len > 14 {
 		return "", -1, errors.New("lenght is error")
 	} else if str_len < 8 {
-		target = fmt.Sprintf("%d%7s", str_len, source)
+		target = fmt.Sprintf("%d%-7s", str_len, source)
         byteArray := []byte(target)
 		target = fmt.Sprintf("%x%x%x%x%x%x%x%x", byteArray[0], byteArray[1],byteArray[2],byteArray[3],byteArray[4],byteArray[5],byteArray[6],byteArray[7])
 		return target, str_len, nil
